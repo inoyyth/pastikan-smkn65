1d468284518be2e0ebf5804a1039fa99e23d130c
Inoy <supri170845@gmail.com>
2026-09-22 23:27:40 +0700

    add favicon

--- index.php
+++ index.php
@@ -129,7 +129,7 @@ if (($_GET['ajax'] ?? '') === 'chart') {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dashboard SMKN 65 Jakarta x Bank Sampah BEO</title>
-  
+  <link rel="icon" type="image/png" href="favicon.png">
   <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link rel="stylesheet" href="styles.css">
